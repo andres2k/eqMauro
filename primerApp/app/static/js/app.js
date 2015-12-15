777147a0fc8efd4ebdb306460152982a95480b83
andres2k <macalvino@hotmail.com>
2015-12-15 14:28:06 -0300

    uniendo angular con django

--- primerApp/app/static/js/app.js
+++ primerApp/app/static/js/app.js
@@ -1,7 +1,36 @@
-    var app = angular.module('primerApp', ['ngRoute']);
+    var app = angular.module('primerApp', ['ngResource', 'ngRoute']);
 
-    app.controller('personaController', function($scope){
-        $scope.name = "Mauro Calviño";
+    app.factory('personaResource', function ($resource) {
+        return $resource('/personas/:id', {id:'@id'},
+        {
+          'get':    {method:'GET', isArray:false},
+          'save':   {method:'POST'},
+          'update': {method:'PATCH'},
+          'query':  {method:'GET', isArray:true},
+          'remove': {method:'DELETE'},
+          'delete': {method:'DELETE'}
+        });
+    });
+
+    app.controller('personaController', function($scope, personaResource){
+        // obtiene las personas
+        function getPersonas(){
+            personaResource.query({}, function(data){
+                $scope.personas = data;
+            });
+        };
+        getPersonas();
+
+
+    });
+
+    app.controller('persona1Controller', function($scope, personaResource, $routeParams){
+        function getPersonasOne(){
+            personaResource.get({id : $routeParams.id }, function(data){
+                $scope.persona1 = data;
+            });
+        };
+        getPersonasOne();
     });
 
     app.config(function($routeProvider) {
@@ -9,6 +38,10 @@
         .when('/', {
             templateUrl : 'templates/personas.html',
             controller  : 'personaController'
+        })
+        .when('/persona/:id', {
+            templateUrl : 'templates/personas_detalle.html',
+            controller  : 'persona1Controller'
         })
         .otherwise({
             redirectTo: '/'
